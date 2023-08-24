d52a65caa7a73a270cf7673f7d8fe7245529f4c2
Steve Russo <steverusso@pm.me>
2023-08-23 22:30:37 -0400

    examples: add new one for positional args and remove partial subcmd one

---
+++ examples/posargs/main.go
@@ -0,0 +1,33 @@
+package main
+
+//go:generate goclap -type mycli
+
+import (
+	"fmt"
+	"os"
+)
+
+// Print a few positional args.
+type mycli struct {
+	// A float32 positional arg.
+	//
+	// clap:arg_required
+	f32 float32
+	// A string positional arg.
+	//
+	// clap:arg_required
+	str string
+	// An uint16 positional arg.
+	//
+	// clap:arg_required
+	u16 uint16
+}
+
+func main() {
+	c := mycli{}
+	c.Parse(os.Args[1:])
+
+	fmt.Printf("f32: %f\n", c.f32)
+	fmt.Printf("str: %s\n", c.str)
+	fmt.Printf("u16: %d\n", c.u16)
+}
